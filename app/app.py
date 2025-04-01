c6a73a00de692aaf3035ee718b7c45f9b9dadd6c
takeru <tata.5593@gmail.com>
2025-04-01 19:34:30 +0900

    パスの変更

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 # app/app.py
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static",template_folder="templates")
 
 @app.route("/")
 def index():
